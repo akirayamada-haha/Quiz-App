455f5e6b99bf57db8ef198956a6d6081bd4206f7
akirayamada-haha <hondamurai99@proton.me>
2019-11-09 19:50:00 -0600

    ready to start jQuery

---
+++ index.js
@@ -0,0 +1,138 @@
+const STORE = [
+    {
+        question: 'Which of the following exercises heavily target the chest muscles?',
+        answers: [
+            'Bicep Curl',
+            'Squat',
+            'Pushup',
+            'Row'
+        ],
+        correctAnswer: 'Pushup'
+    },
+    {
+        question: 'Which exercise is considered the “king” of all exercises?',
+        answers: [
+            'Bench Press',
+            'Lunge',
+            'Deadlift',
+            'Squat'
+        ],
+        correctAnswer: 'Squat'
+    },
+    {
+        question: 'Which Rep Ranges most are associated with maximal strength?',
+        answers: [
+            '1 - 3',
+            '5',
+            '8 - 12',
+            '12 - 15'
+        ],
+        correctAnswer: '1 - 3'
+    },
+    {
+        question: 'Which of the following is considered a “core” exercise?',
+        answers: [
+            'Plank',
+            'Situp',
+            'Superman',
+            'All of the above'
+        ],
+        correctAnswer: 'All of the above'
+    },
+    {
+        question: 'Which macronutrient is a building block for muscle?',
+        answers: [
+            'Fat',
+            'Protein',
+            'Carbohydrate',
+            'Alcohol'
+        ],
+        correctAnswer: 'Protein'
+    },
+];
+
+//need score and counter set to starting value
+
+//on clicking start, replace landing page html with question 1 html, pulled from store
+
+//generate question 1 html
+
+//render question 1 html
+
+//when answer is submitted, check against correctAnswer
+
+//based on answer selected, when submit is clicked, give feedback and update html with next button
+
+//generate feedback
+
+//render feedback
+
+//update score and counter
+
+//on clicking next, replace question 1 page html with question 2 html, pulled from store
+
+//generate question 2 html
+
+//render question 2 html
+
+//when answer is submitted, check against correctAnswer
+
+//based on answer selected, when submit is clicked, give feedback and update html with next button
+
+//generate feedback
+
+//render feedback
+
+//update score and counter
+
+//on clicking next, replace question 2 page html with question 3 html, pulled from store
+
+//generate question 3 html
+
+//render question 3 html
+
+//when answer is submitted, check against correctAnswer
+
+//based on answer selected, when submit is clicked, give feedback and update html with next button
+
+//generate feedback
+
+//render feedback
+
+//update score and counter
+
+//on clicking next, replace question 3 page html with question 4 html, pulled from store
+
+//generate question 4 html
+
+//render question 4 html
+
+//when answer is submitted, check against correctAnswer
+
+//based on answer selected, when submit is clicked, give feedback and update html with next button
+
+//generate feedback
+
+//render feedback
+
+//update score and counter
+
+//on clicking next, replace question 4 page html with question 5 html, pulled from store
+
+//generate question 5 html
+
+//render question 5 html
+
+//when answer is submitted, check against correctAnswer
+
+//based on answer selected, when submit is clicked, give feedback and update html with finish button
+
+//generate feedback
+
+//render feedback
+
+//Upon clicking finish, replace question 5 page html with final page html and restart button
+
+//generate final page
+
+//render final page
